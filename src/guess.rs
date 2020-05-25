4014b2ace8908e894fe537754c11ac684892a432
Ivaylo Novakov <inovakov@gmail.com>
2020-05-25 09:38:36 +0200

    Move GCD funcs to a lib.

---
+++ src/guess.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+
+fn guess_num() {
+  let num = rand::thread_rng().gen_range(0, 100);
+  println!("Guess the number!");
+  loop {
+    let mut guess = String::new();
+    stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read the guess!");
+    let guess_num = match guess.trim().parse::<i32>() {
+      Ok(n) => n,
+      Err(_) => {
+        println!("Enter a number!");
+        continue;
+      }
+    };
+
+    match guess_num.cmp(&num) {
+      Ordering::Less => println!("Too low!"),
+      Ordering::Greater => println!("Too high!"),
+      Ordering::Equal => {
+        println!("You guessed it!");
+        break;
+      }
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
 #![allow(dead_code)]
 
-use std::cmp::Ordering;
-use std::io::stdin;
-
-use rand::Rng;
-
-fn guess_num() {
-    let num = rand::thread_rng().gen_range(0, 100);
-    println!("Guess the number!");
-    loop {
-        let mut guess = String::new();
-        stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read the guess!");
-        let guess_num = match guess.trim().parse::<i32>() {
-            Ok(n) => n,
-            Err(_) => {
-                println!("Enter a number!");
-                continue;
-            }
-        };
-
-        match guess_num.cmp(&num) {
-            Ordering::Less => println!("Too low!"),
-            Ordering::Greater => println!("Too high!"),
-            Ordering::Equal => {
-                println!("You guessed it!");
-                break;
-            }
-        }
-    }
-}
-
 fn hello() -> &'static str {
     &"hello"
 }
